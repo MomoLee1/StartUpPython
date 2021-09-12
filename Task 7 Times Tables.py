5f242dd0be65cc553d7b5000137976365c69d300
MomoLee1 <leem@dulwich.org.uk>
2021-09-12 16:31:33 +0100

    Task 7 Times Tables

--- Task 7 Times Tables.py
+++ Task 7 Times Tables.py
@@ -3,7 +3,7 @@ number = float(input("Input a number between 1-10"))
 if number > 0 and number < 11:
     while i < 11:
         print(number*i)
-        i = i + 1
+        i = i + 1 
     #endwhile
 #endif
 
