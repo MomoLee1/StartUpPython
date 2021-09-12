ffd519c1b22030a8f4fac95d7d19062e072866ce
MomoLee1 <leem@dulwich.org.uk>
2021-09-12 15:21:47 +0100

    Task 4 division

---
+++ Task 4 division.py
@@ -0,0 +1,5 @@
+#user enters integer and divisor
+integer = input ("Input an integer")
+divisor = input ("Input a divisor")
+#output whole integer division and remainder
+print (integer//divisor, " remainder", integer%divisor)
