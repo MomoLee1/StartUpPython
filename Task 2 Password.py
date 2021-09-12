aa113dbbc88fda559e03005f2c8db6d0c05742ce
MomoLee1 <leem@dulwich.org.uk>
2021-09-12 13:59:23 +0100

    Task 2 Password

---
+++ Task 2 Password.py
@@ -0,0 +1,8 @@
+#get user to input password
+password = input("Input a password")
+#check if password is more than 6 characters
+if len(password) > 6:
+    print("Password is valid")
+else:
+    print("Password is invalid")
+#endif 
