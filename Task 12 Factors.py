13dd895d24dc5c91ae57dfd63fd268cb3b2aba79
MomoLee1 <leem@dulwich.org.uk>
2021-09-14 09:39:25 +0100

    Task 12 Factors

--- Task 12 Factors.py
+++ Task 12 Factors.py
@@ -3,10 +3,10 @@ n = 1
 x = int(input("Input an integer"))
 print("The factors of ", x, "are: ")
 while n <= x:
-    if x % n == 0:
+    if x%n == 0:
         print(n)
-        n=n+1
     #endif
+    n=n+1
 #endwhile
 
 
