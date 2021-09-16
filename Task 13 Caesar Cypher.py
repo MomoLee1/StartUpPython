57d6c6a8e9f2af1a43862cff08b2d9c963b57793
MomoLee1 <leem@dulwich.org.uk>
2021-09-17 00:00:25 +0100

    Task 13 Caesar Cypher

--- Task 13 Caesar Cypher.py
+++ Task 13 Caesar Cypher.py
@@ -1,4 +1,13 @@
 #user inputs string
-sentence = input("Type in a sentence")
+sentence = input("Type in a sentence").lower()
+#user inputs shift
 shift = int(input("What is your shift?"))
-
+#initialise empty list
+cipheredmessage=""
+#loop for every character in string
+for ch in sentence:
+    #cipher
+    cipheredmessage+= chr(ord(ch) + shift) 
+#end for
+#output ciphered message
+print(cipheredmessage)
